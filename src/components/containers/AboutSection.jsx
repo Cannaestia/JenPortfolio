060addf4929c5dc66dbb4199d388eed0bc9b57a3
Jennifer Arnold <cannaestia@gmail.com>
2023-03-14 23:09:49 -0400

    updated resume

--- src/components/containers/AboutSection.jsx
+++ src/components/containers/AboutSection.jsx
@@ -112,7 +112,7 @@ const AboutSection = () => {
               </li>
             )}
           </ul>
-          <a href="/JenREsume2023Updated.pdf" className="btn mt-3">
+          <a href="/JenMarchResume.pdf" className="btn mt-3">
             <span>Download Resume</span>
           </a>
         </motion.div>
